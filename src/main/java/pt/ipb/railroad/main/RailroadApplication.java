aa1a29ef9ce817f179307cf50975cd5238d61fa4
Arthur Falcão <arthurfalcao77@gmail.com>
2020-04-09 13:51:15 +0100

    refactor: rename main application

--- src/main/java/pt/ipb/railroad/main/RailroadApplication.java
+++ src/main/java/pt/ipb/railroad/main/RailroadApplication.java
@@ -1,6 +1,6 @@
 package main.java.pt.ipb.railroad.main;
 
-public class Railroad {
+public class RailroadApplication {
 
     public static void main(String[] args) {
         System.out.println("Railroad");
